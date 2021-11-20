6ce633d364bec67563806112b26fdc5e00e03d10
ThreadedStream <frankflipper3@gmail.com>
2021-11-20 16:18:58 +0300

    added some ast node classes

--- prss_no_antlr/main.cpp
+++ prss_no_antlr/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <optional>
 
 #include <cstdint>
+#include <numeric>
+#include "antlr4-runtime.h"
+
+template<typename T>
+using opt<T> = std::optional<T>;
 
 // TODO(threadedstream): quicky build a lexer and expression flattener
 
 // just copied it from Python3Lexer.h
-enum TokenType : uint8_t{
+enum class TokenType : uint8_t {
     STRING = 1, NUMBER = 2, INTEGER = 3, DEF = 4, RETURN = 5, RAISE = 6,
     FROM = 7, IMPORT = 8, AS = 9, GLOBAL = 10, NONLOCAL = 11, ASSERT = 12,
     IF = 13, ELIF = 14, ELSE = 15, WHILE = 16, FOR = 17, IN = 18, TRY = 19,
@@ -29,11 +35,15 @@ enum TokenType : uint8_t{
     UNKNOWN_CHAR = 97
 };
 
+enum class AssignFlag : uint8_t {
+    OP_ASSIGN = 0,
+};
+
 struct Token {
     int32_t line;
     int32_t column;
     TokenType type;
-    const char* text;
+    const char *text;
 };
 
 class Lexer {
@@ -50,10 +60,10 @@ public:
 
     char LookAhead(int32_t n) {
         if (curr_idx + n < source_.size()) { return EOF; }
-        return source_[curr_idx+n];
+        return source_[curr_idx + n];
     }
 
-    Token ProduceToken(const std::string& name, int32_t type) {
+    Token ProduceToken(const std::string &name, int32_t type) {
 
     }
 
@@ -66,11 +76,96 @@ private:
 
 // AST nodes for P0 subset
 
-struct Node{
+struct Node {
+    // TODO(threadedstream): fill in the rest
+
+    virtual ~Node();
 };
 
-struct Module {
+struct Module : public Node {
+    Module(const std::string &doc, Node *node) : node_(node), doc_(doc) {}
 
+    Node *node_;
+    std::string doc_;
+};
+
+struct Stmt : public Node {
+    Stmt(std::vector<Node*> nodes) : nodes_(nodes) {
+    }
+
+    std::vector<Node*> nodes_;
+};
+
+struct Printnl : public Node {
+    Printnl(Node *nodes, Node *dest) : nodes_(nodes), dest_(dest) {}
+
+    Node *nodes_;
+    Node *dest_;
+};
+
+struct Assign : public Node {
+    Assign(std::vector<Node*> nodes, Node *expr) : nodes_(nodes), expr_(expr) {}
+
+    std::vector<Node*> nodes_;
+    Node *expr_;
+};
+
+struct AssName : public Node {
+    AssName(const std::string &name, AssignFlag flags) : name_(name), flags_(flags) {}
+
+    std::string name_;
+    AssignFlag flags_;
+};
+
+struct Discard : public Node {
+    Discard(Node *expr) : expr_(expr) {}
+
+    Node *expr_;
+};
+
+struct Const : public Node {
+    Const(const antlrcpp::Any &value) : value_(value) {}
+
+    antlrcpp::Any value_;
+};
+
+struct Name : public Node {
+    Name (const std::string& name) : name_(name) {}
+
+    std::string name_;
+};
+
+struct Add : public Node {
+    Add (Node* left, Node* right) : left_(left), right_(right) {}
+
+    Node* left_;
+    Node* right_;
+};
+
+struct UnarySub : public Node {
+    UnarySub (Node* expr) : expr_(expr) {}
+
+    Node* expr_;
+};
+
+struct CallFunc : public Node {
+    CallFunc (Node* node, Node* args) : node_(node), args_(args){}
+
+    Node* node_;
+    Node* args_;
 };
 
 
+// TODO(threadedstream): To be finished. Should really be thinking of better way to write this function
+constexpr int32_t AstNumNodes(Node* node) {
+    // convert that to type switch
+    if constexpr (auto mod = dynamic_cast<Module*>(node)) {
+        return 1 + AstNumNodes(mod->node_);
+    } else if constexpr (auto stmt = dynamic_cast<Stmt*>(node)) {
+        std::vector<int32_t> children_node_nums;
+        for (const auto &node : stmt->nodes_) {
+            children_node_nums.push_back(AstNumNodes(node));
+        }
+        return 1 + std::accumulate(children_node_nums.begin(), children_node_nums.end(), 0);
+    }
+}
